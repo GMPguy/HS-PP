cbe156ccab6778d44857cca5800fcb69467ed012
GMPguy <45078814+GMPguy@users.noreply.github.com>
2024-10-25 14:49:24 +0200

    Garand and gun fires

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,8 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 public class MainMenuUI : UITemplate {
+
+    // References
+    [SerializeField]
+    RectTransform MenuButtons;
 
     public override void ClearUp() {Cleared = true;}
 
-    public override void SetUp(int addition) {}
+    public override void SetUp(int addition) {
+
+        
+
+    }
 
     public override void UIUpdate() {}
 
